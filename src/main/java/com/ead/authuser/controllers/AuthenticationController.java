b12c96df347f75a31e97dfc5bab1b70a3bd54c0c
Mario Miranda <mario_chan15@hotmail.com>
2022-10-17 11:39:17 -0400

    insert register user admin api

--- src/main/java/com/ead/authuser/controllers/AuthenticationController.java
+++ src/main/java/com/ead/authuser/controllers/AuthenticationController.java
@@ -85,6 +85,47 @@ public class AuthenticationController {
         return ResponseEntity.status(HttpStatus.CREATED).body(userModel);
     }
 
+    @PostMapping("/signup/admin/usr")
+    public ResponseEntity<Object> registerUserAdmin(
+            @RequestBody
+            @Validated(UserDto.UserView.RegistrationPost.class)
+            @JsonView(UserDto.UserView.RegistrationPost.class)
+            UserDto userDto) {
+
+        log.debug("POST registerUserAdmin userDto received {} ", userDto.toString());
+
+        if(userService.existsByUsername(userDto.getUsername())) {
+            log.warn("POST registerUser Username {} is already taken!", userDto.getUsername());
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: Username is already taken!");
+        }
+
+        if(userService.existsByEmail(userDto.getEmail())) {
+            log.warn("POST registerUser Email {} is already taken!", userDto.getEmail());
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: Email is already taken!");
+        }
+
+        RoleModel roleModel = roleService.findByRoleName(RoleType.ROLE_ADMIN)
+                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+
+        userDto.setPassword(passwordEncoder.encode(userDto.getPassword()));
+
+        var userModel = new UserModel();
+        BeanUtils.copyProperties(userDto, userModel);
+        userModel.setUserStatus(UserStatus.ACTIVE);
+        userModel.setUserType(UserType.ADMIN);
+        userModel.setCreationDate(LocalDateTime.now(ZoneId.of("UTC")));
+        userModel.setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
+
+        userModel.getRoles().add(roleModel);
+
+        userService.saveUser(userModel);
+
+        log.debug("POST registerUser userModel saved userId {} ", userModel.getUserId());
+        log.info("User saved successfully {}", userModel.getUserId());
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(userModel);
+    }
+
     @PostMapping("/login")
     public ResponseEntity<JwtDto> authenticateUser(
             @Validated
